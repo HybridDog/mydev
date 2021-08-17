6aac818c561355902416940eb47556d409c9e2a1
HybridDog <ovvv@web.de>
2021-08-17 19:52:56 +0200

    add unfinished 3D hilbert curve

--- spline_caves.lua
+++ spline_caves.lua
@@ -347,6 +347,199 @@ worldedit.register_command("splc", {
 
 
 
+local hilbert_table_3d
+do
+	local function strreplace(str, a, b)
+		if not str then
+			return str
+		end
+		return str:gsub(a, "P"):gsub(b, a):gsub("P", b)
+	end
+	local function strrot(str, a, b, c, d)
+		if not str then
+			return str
+		end
+		return str:gsub(a, "P"):gsub(b, a):gsub(c, b):gsub(d, c):gsub("P", d)
+	end
+	local function onallstr(t, func)
+		for i = 1, #t do
+			t[i] = {func(t[i][1]), func(t[i][2]), t[i][3]}
+		end
+	end
+	local function flipz(t)
+		local t_new = {}
+		for i = 1,4 do
+			t_new[i] = t[4 + i]
+			t_new[4 + i] = t[i]
+		end
+		for i = 1,4 do
+			t_new[i] = t[4 + i]
+			t_new[4 + i] = t[i]
+		end
+		onallstr(t_new, function(str)
+			return strreplace(str, "b", "f")
+		end)
+		return t_new
+	end
+	local function flipx(t)
+		local t_new = {}
+		for i = 1, 4 do
+			t_new[2 * i - 1] = t[2 * i]
+			t_new[2 * i] = t[2 * i - 1]
+		end
+		onallstr(t_new, function(str)
+			return strreplace(str, "r", "l")
+		end)
+		return t_new
+	end
+	local function flipy(t)
+		local t_new = {}
+		local perm = {3, 4, 1, 2, 7, 8, 5, 6}
+		for i = 1, 8 do
+			t_new[i] = t[perm[i]]
+		end
+		onallstr(t_new, function(str)
+			return strreplace(str, "u", "d")
+		end)
+		return t_new
+	end
+	-- 90 degrees counterclockwise
+	local function roty(t)
+		local t_new = {}
+		local perm = {5, 1, 7, 3, 6, 2, 8, 4}
+		for i = 1, 8 do
+			t_new[i] = t[perm[i]]
+		end
+		onallstr(t_new, function(str)
+			return strrot(str, "l", "b", "r", "f")
+		end)
+		return t_new
+	end
+	local function rotx(t)
+		local t_new = {}
+		local perm = {3, 4, 7, 8, 1, 2, 5, 6}
+		for i = 1, 8 do
+			t_new[i] = t[perm[i]]
+		end
+		onallstr(t_new, function(str)
+			return strrot(str, "f", "d", "b", "u")
+		end)
+		return t_new
+	end
+
+	hilbert_table_3d = {
+		bru = {{"rub", "b", 0}, {"flu", "u", 3},
+			{"rdf", nil, 7}, {"flu", "b", 4},
+			{"ubr", "r", 1}, {"ubr", "f", 2}, {"dbl", "f", 6}, {"dbl", "l", 5}},
+	}
+	hilbert_table_3d.flu = flipx(flipz(hilbert_table_3d.bru))
+	hilbert_table_3d.bld = flipx(flipy(hilbert_table_3d.bru))
+	hilbert_table_3d.frd = flipz(flipy(hilbert_table_3d.bru))
+	hilbert_table_3d.dbl = roty(rotx(hilbert_table_3d.bru))
+	hilbert_table_3d.ubr = flipx(flipy(hilbert_table_3d.dbl))
+	hilbert_table_3d.ufl = flipz(flipy(hilbert_table_3d.dbl))
+	hilbert_table_3d.dfr = flipz(flipx(hilbert_table_3d.dbl))
+	hilbert_table_3d.rub = rotx(roty(hilbert_table_3d.flu))
+	hilbert_table_3d.rdf = flipy(flipz(hilbert_table_3d.rub))
+	hilbert_table_3d.luf = flipx(flipz(hilbert_table_3d.rub))
+	hilbert_table_3d.ldb = flipy(flipx(hilbert_table_3d.rub))
+end
+-- TODO: what about Minetest's left-handed coordinate system?
+
+local function hilbert_3d_get_direction(component_prev, dir_prev, x, y, z,
+		level)
+	if level == 0 then
+		return dir_prev
+	end
+	x = 2 * x
+	y = 2 * y
+	z = 2 * z
+	local i = 0
+	if x >= 1 then
+		i = i + 1
+		x = x - 1
+	end
+	if y >= 1 then
+		i = i + 2
+		y = y - 1
+	end
+	if z >= 1 then
+		i = i + 4
+		z = z - 1
+	end
+	local data = hilbert_table_3d[component_prev][i+1]
+	return hilbert_3d_get_direction(data[1], data[2] or dir_prev,
+		x, y, z, level-1)
+end
+
+local HilbertCurve3D = {}
+setmetatable(HilbertCurve3D, {__call = function(_, size)
+	local num_levels = math.ceil(math.log(size) / math.log(2))
+	local obj = {
+		num_levels = num_levels,
+		directions_cache = {}
+	}
+	setmetatable(obj.directions_cache, {__mode = "kv"})
+	setmetatable(obj, HilbertCurve3D)
+	return obj
+end})
+HilbertCurve3D.__index = {
+	-- Returns a direction ("r", "l", "u", "d", "b", "f") where the curve exits
+	-- at (x, y, z).
+	-- ("r", "l", "u", "d", "b", "f") stand for (+X, -X, +Y, -Y, +Z, -Z), i.e.
+	-- (right, left, up, down, backwards, forwards)
+	-- (x, y, z) should be in {0, 1, …, size-1}^3
+	get_out_direction = function(self, x, y, z)
+		local size = 2 ^ self.num_levels
+		assert(x >= 0 and y >= 0 and z >= 0
+			and x < size and y < size and z < size)
+		local vi = (z * size + y) * size + x
+		local dir = self.directions_cache[vi]
+		if dir then
+			return dir
+		end
+		-- I chose the first level of the curve to go bbrfublff
+		dir = hilbert_3d_get_direction("bru", "b", x / size, y / size, z / size,
+			self.num_levels)
+		self.directions_cache[vi] = dir
+		return dir
+	end,
+
+	-- Returns a direction where the curve enters and exits at (x, y, z).
+	-- Entering means exiting from a neighbouring position
+	get_in_and_out_direction = function(self, x, y, z)
+		local size = 2 ^ self.num_levels
+		local off = {l = {-1, 0, 0}, r = {1, 0, 0}, d = {0, -1, 0},
+			u = {0, 1, 0}, f = {0, 0, -1}, b = {0, 0, 1}}
+		local dir_out = self:get_out_direction(x, y, z)
+		local opposites = {r = "l", l = "r", u = "d", d = "u", f = "b", b = "f"}
+		for dir, vec in pairs(off) do
+			if dir ~= dir_out then
+				local xo = x + vec[1]
+				local yo = y + vec[2]
+				local zo = z + vec[3]
+				if xo >= 0 and yo >= 0 and zo >= 0
+						and xo < size and yo < size and zo < size then
+					local dir_out_neigh = self:get_out_direction(xo, yo, zo)
+					if dir_out_neigh == opposites[dir] then
+						-- Found a point which leads to (x, y, z)
+						return opposites[dir_out_neigh], dir_out
+					end
+				end
+			end
+		end
+		-- TODO: this fails
+		-- Rare edge case: the beginning of the curve
+		--~ assert(x == 0 and y == 0 and z == 0, "Couldn't find in for (" .. x ..
+			--~ ", " .. y .. ", " .. z .. ")")
+		-- The first level of the curve goes back, […]
+		-- -> the first input direction is front
+		return "f", dir_out
+	end,
+}
+
+
+
 
 -- A table which defines the Hilbert curve recursively. The rows are ordered
 -- for offsets (x,y) in (0,0), (1,0), (0,1), (1,1) (if the offsets are unscaled)
@@ -442,24 +635,52 @@ HilbertCurve2D.__index = {
 }
 
 -- a testing function for the hilbert 2d curve
-local function get_2d_hilbert_nodes(pos1, pos2)
+--~ local function get_2d_hilbert_nodes(pos1, pos2)
+	--~ local nodes = {}
+	--~ local size = math.ceil(
+		--~ math.max(pos2.x - pos1.x, pos2.y - pos1.y, pos2.z - pos1.z) / 3)
+	--~ local hilb = HilbertCurve2D(size)
+	--~ for i = 0, size-1 do
+		--~ for j = 0, size-1 do
+			--~ local dir_in, dir_out = hilb:get_in_and_out_direction(i, j)
+			--~ local x = pos1.x + 3 * i
+			--~ local z = pos1.z + 3 * j
+			--~ nodes[#nodes+1] = {{x=x+1, y=pos1.y, z=z+1}, "default:mese"}
+			--~ local off = {l = {-1, 0}, r = {1, 0}, d = {0, -1}, u = {0, 1}}
+			--~ local vec_in = off[dir_in]
+			--~ local vec_out = off[dir_out]
+			--~ nodes[#nodes+1] = {{x=x+vec_in[1]+1, y=pos1.y, z=z+vec_in[2]+1},
+				--~ "default:stone"}
+			--~ nodes[#nodes+1] = {{x=x+vec_out[1]+1, y=pos1.y, z=z+vec_out[2]+1},
+				--~ "default:cobble"}
+		--~ end
+	--~ end
+	--~ return nodes
+--~ end
+
+-- a testing function for the hilbert 3d curve
+local function get_3d_hilbert_nodes(pos1, pos2)
 	local nodes = {}
 	local size = math.ceil(
 		math.max(pos2.x - pos1.x, pos2.y - pos1.y, pos2.z - pos1.z) / 3)
-	local hilb = HilbertCurve2D(size)
+	local hilb = HilbertCurve3D(size)
 	for i = 0, size-1 do
 		for j = 0, size-1 do
-			local dir_in, dir_out = hilb:get_in_and_out_direction(i, j)
-			local x = pos1.x + 3 * i
-			local z = pos1.z + 3 * j
-			nodes[#nodes+1] = {{x=x+1, y=pos1.y, z=z+1}, "default:mese"}
-			local off = {l = {-1, 0}, r = {1, 0}, d = {0, -1}, u = {0, 1}}
-			local vec_in = off[dir_in]
-			local vec_out = off[dir_out]
-			nodes[#nodes+1] = {{x=x+vec_in[1]+1, y=pos1.y, z=z+vec_in[2]+1},
-				"default:stone"}
-			nodes[#nodes+1] = {{x=x+vec_out[1]+1, y=pos1.y, z=z+vec_out[2]+1},
-				"default:cobble"}
+			for k = 0, size-1 do
+				local dir_in, dir_out = hilb:get_in_and_out_direction(i, j, k)
+				local x = pos1.x + 3 * i
+				local y = pos1.y + 3 * j
+				local z = pos1.z + 3 * k
+				nodes[#nodes+1] = {{x=x+1, y=y+1, z=z+1}, "default:mese"}
+				local off = {l = {-1, 0, 0}, r = {1, 0, 0}, d = {0, -1, 0},
+					u = {0, 1, 0}, f = {0, 0, -1}, b = {0, 0, 1}}
+				local vec_in = off[dir_in]
+				local vec_out = off[dir_out]
+				nodes[#nodes+1] = {{x=x+vec_in[1]+1, y=y+vec_in[2]+1,
+					z=z+vec_in[3]+1}, "default:stone"}
+				nodes[#nodes+1] = {{x=x+vec_out[1]+1, y=y+vec_out[2]+1,
+					z=z+vec_out[3]+1}, "default:cobble"}
+			end
 		end
 	end
 	return nodes
@@ -472,8 +693,8 @@ worldedit.register_command("hilb", {
 	require_pos = 1,
 	func = function(playername)
 		local pos1 = worldedit.pos1[playername]
-		local pos2 = vector.add(pos1, 3*32)
-		local nodes = get_2d_hilbert_nodes(pos1, pos2)
+		local pos2 = vector.add(pos1, 3*4)
+		local nodes = get_3d_hilbert_nodes(pos1, pos2)
 		simple_vmanip(nodes)
 
 		return true
